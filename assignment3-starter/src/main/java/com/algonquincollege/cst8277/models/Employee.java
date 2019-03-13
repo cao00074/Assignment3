dbac4487119541159c3e6acd0063c32aaf1af210
cao00074 <cao00074@algonquinlive.com>
2019-03-13 02:00:20 +0000

    to combine xiaoxiao's work

--- assignment3-starter/src/main/java/com/algonquincollege/cst8277/models/Employee.java
+++ assignment3-starter/src/main/java/com/algonquincollege/cst8277/models/Employee.java
@@ -18,7 +18,12 @@ package com.algonquincollege.cst8277.models;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
 
 /**
  * The Employee class demonstrates several JPA features:
@@ -31,17 +36,51 @@ import javax.persistence.Entity;
  * </ul>
  */
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "findAllEmployeesWithSIm",
+            query="select e from Employee e where e.lastName like :lastNmae")
+})
 public class Employee extends ModelBase implements Serializable {
     /** explicit set serialVersionUID */
     private static final long serialVersionUID = 1L;
 
     // TODO - additional persistent field
-
+    protected String firstName;
+    protected String lastName;
+    protected double salary;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id",referencedColumnName = "id" )
+    protected Address address;
+ 
     // JPA requires each @Entity class have a default constructor
     public Employee() {
         super();
     }
-
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+    
     // Strictly speaking, JPA does not require hashcode() and equals(),
     // but it is a good idea to have one that tests using the PK (@Id) field
 
