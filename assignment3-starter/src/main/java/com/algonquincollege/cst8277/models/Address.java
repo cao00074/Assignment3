cfe80d09629fe2e7dd54cccd5eab74047a693755
cao00074 <cao00074@algonquinlive.com>
2019-03-13 01:52:54 +0000

    my first commit

---
+++ assignment3-starter/src/main/java/com/algonquincollege/cst8277/models/Address.java
@@ -0,0 +1,129 @@
+/********************************************************************egg***m******a**************n************
+ * File: Address.java
+ * Course materials (19W) CST 8277
+ * @author Mike Norman
+ * (Modified) @date 2019 03
+ *
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Original @authors dclarke, mbraeuer
+ *
+ */
+package com.algonquincollege.cst8277.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Simple Address class - it uses a generated Id.
+ */
+@Entity
+@Table(name = "address")
+public class Address extends ModelBase implements Serializable {
+    /** explicit set serialVersionUID */
+    private static final long serialVersionUID = 1L;
+
+    // TODO - additional persistent field
+    protected String city;
+    protected String country;
+    protected String postal;
+    protected String state;
+    protected String street;
+    //protected int addrVersion;
+    
+    @OneToOne(mappedBy = "address")
+    private Employee employee;
+    
+    // JPA requires each @Entity class have a default constructor
+    public Address() {
+        super();
+    }
+
+ 
+    public String getCity() {
+        return city;
+    }
+
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    public String getPostal() {
+        return postal;
+    }
+
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+
+    public String getState() {
+        return state;
+    }
+
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+    public String getStreet() {
+        return street;
+    }
+
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    // Strictly speaking, JPA does not require hashcode() and equals(),
+    // but it is a good idea to have one that tests using the PK (@Id) field
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Address)) {
+            return false;
+        }
+        Address other = (Address)obj;
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ assignment3-starter/src/main/java/com/algonquincollege/cst8277/models/Phone.java
@@ -0,0 +1,115 @@
+/********************************************************************egg***m******a**************n************
+ * File: Phone.java
+ * Course materials (19W) CST 8277
+ * @author Mike Norman
+ * (Modified) @date 2019 03
+ *
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Original @authors dclarke, mbraeuer
+ */
+package com.algonquincollege.cst8277.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * The Phone class demonstrates:
+ * <ul>
+ * <li>Generated Id
+ * <li>Version locking
+ * <li>ManyToOne mapping
+ * </ul>
+ */
+@Entity
+@Table(name = "phone")
+public class Phone extends ModelBase implements Serializable {
+    /** explicit set serialVersionUID */
+    private static final long serialVersionUID = 1L;
+
+    // TODO - persistent fields
+    //protected int id;
+    protected String type;
+    protected String areaCode;
+    protected String phoneNumber;
+    //protected int phoneVersion;
+    
+    
+    @ManyToOne
+    @JoinColumn(name = "empId", nullable = false)
+    protected Employee employee;
+
+    
+
+    // JPA requires each @Entity class have a default constructor
+    public Phone() {
+        super();
+    }
+
+    // Strictly speaking, JPA does not require hashcode() and equals(),
+    // but it is a good idea to have one that tests using the PK (@Id) field
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+  
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Phone)) {
+            return false;
+        }
+        Phone other = (Phone)obj;
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
+}
