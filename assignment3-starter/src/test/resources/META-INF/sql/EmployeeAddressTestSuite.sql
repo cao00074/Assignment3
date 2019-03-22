1cdbd2a12197333eee6ba5f41ae1bde50a686e99
cao00074 <cao00074@algonquinlive.com>
2019-03-22 21:11:03 +0000

    add pre-condition for testing

--- assignment3-starter/src/test/resources/META-INF/sql/EmployeeAddressTestSuite.sql
+++ assignment3-starter/src/test/resources/META-INF/sql/EmployeeAddressTestSuite.sql
@@ -1,2 +1,4 @@
 -- any sql that is to be pre-loaded before testsuite runs goes in here
 -- statements must be a single line without returns
+INSERT INTO ADDRESS (ID, CITY, COUNTRY, POSTAL, STATE, STREET, VERSION) VALUES (1, 'Nepean', 'CA', 'K2X 5A1', 'ON', '20 Pinetrail Cress', 1)
+INSERT INTO EMPLOYEE (ID, FIRSTNAME, LASTNAME, SALARY, VERSION, ADDR_ID) VALUES (1, 'Mike', 'Norman', '80000', '2', 1)
