08f3e51333c5c457fd472c9cf16efc67f14560b2
cao00074 <cao00074@algonquinlive.com>
2019-03-24 18:09:32 +0000

    merge Xiaoxiao's change

--- assignment3-starter/src/test/resources/META-INF/sql/EmployeePhonesTestSuite.sql
+++ assignment3-starter/src/test/resources/META-INF/sql/EmployeePhonesTestSuite.sql
@@ -1,2 +1,4 @@
 -- any sql that is to be pre-loaded before testsuite runs goes in here
 -- statements must be a single line without returns
+INSERT INTO ADDRESS (ID, CITY, COUNTRY, POSTAL, STATE, STREET, VERSION) VALUES (5, 'Toronto', 'CA', 'K1B 9Y6', 'ON', '1 Main Street', 1)
+INSERT INTO EMPLOYEE (ID, FIRSTNAME, LASTNAME, SALARY, VERSION, ADDR_ID) VALUES(10, 'Lei', 'Cao', 5000, 1, 5)
