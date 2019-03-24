3c8812b697103b7ce17d5c7b9dad31ff401410ae
cao00074 <cao00074@algonquinlive.com>
2019-03-24 16:23:15 +0000

    updated inserting data

--- assignment3-starter/src/test/resources/META-INF/sql/EmployeeAddressTestSuite.sql
+++ assignment3-starter/src/test/resources/META-INF/sql/EmployeeAddressTestSuite.sql
@@ -1,5 +1,5 @@
 -- any sql that is to be pre-loaded before testsuite runs goes in here
 -- statements must be a single line without returns
 INSERT INTO ADDRESS (ID, CITY, COUNTRY, POSTAL, STATE, STREET, VERSION) VALUES (1, 'Nepean', 'CA', 'K2X 5A1', 'ON', '20 Pinetrail Cress', 1)
-INSERT INTO ADDRESS (ID, CITY, COUNTRY, POSTAL, STATE, STREET, VERSION) VALUES (3, 'Carp', 'CA', 'K0A 1L0', 'ON', '185 Charlie's Lane', 1)
+INSERT INTO ADDRESS (ID, CITY, COUNTRY, POSTAL, STATE, STREET, VERSION) VALUES (3, 'Carp', 'CA', 'K0A 1L0', 'ON', '185 Charlies Lane', 1)
 INSERT INTO EMPLOYEE (ID, FIRSTNAME, LASTNAME, SALARY, VERSION, ADDR_ID) VALUES (1, 'Mike', 'Norman', '80000', '2', 1)
